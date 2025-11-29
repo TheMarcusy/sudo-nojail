3804ee315b5f9c5971f3cda58100f40c5362d0a4
TheMarcusy <beeruncut@gmail.com>
2025-11-30 05:27:12 +0700

    Refactor main function and update error handling

--- pseudo.m
+++ pseudo.m
@@ -1,35 +1,43 @@
-commandseudoseudoimport Darwin;
-extern char** environ;
+#import <Foundation/Foundation.h>
+#import <spawn.h>
+#import <errno.h>
+#import <string.h>
+#import <stdio.h>
+#import <unistd.h>
+#import <sys/wait.h>
+
+extern char **environ;
 
 #define POSIX_SPAWN_PERSONA_FLAGS_OVERRIDE 1
-int posix_spawnattr_set_persona_np(const posix_spawnattr_t* __restrict, uid_t, uint32_t);
-int posix_spawnattr_set_persona_uid_np(const posix_spawnattr_t* __restrict, uid_t);
-int posix_spawnattr_set_persona_gid_np(const posix_spawnattr_t* __restrict, uid_t);
 
-int main(int argc, char** argv) {
-  if (argc < 2) {
-    fprintf(stderr, "usage: ido <command> <to> <run>\n");
-    return 1;
-  }
-  
-  if (strcmp(argv[1], "--version") == 0) {
-    fprintf(stdout, "ido 1.0 – A sudo alternative for iOS\n");
-    fprintf(stdout, "(c) 2023 BomberFish Industries.\n");
-    return 0;
-  }
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: ido <command> <args...>\n");
+        return 1;
+    }
 
-  posix_spawnattr_t attr;
-  posix_spawnattr_init(&attr);
-  posix_spawnattr_set_persona_np(&attr, /*persona_id=*/99, POSIX_SPAWN_PERSONA_FLAGS_OVERRIDE);
-  posix_spawnattr_set_persona_uid_np(&attr, 0);
-  posix_spawnattr_set_persona_gid_np(&attr, 0);
+    if (strcmp(argv[1], "--version") == 0) {
+        fprintf(stdout, "ido 1.0 – A sudo alternative for iOS\n");
+        fprintf(stdout, "(c) 2023 BomberFish Industries.\n");
+        return 0;
+    }
 
-  int pid = 0;
-  int ret = posix_spawnp(&pid, argv[1], NULL, &attr, &argv[1], environ);
-  if (ret) {
-    fprintf(stderr, "failed to exec %s: %s\n", argv[1], strerror(errno));
-    return 1;
-  }
-  waitpid(pid, nil, 0);
-  return 0;
+    posix_spawnattr_t attr;
+    posix_spawnattr_init(&attr);
+    posix_spawnattr_set_persona_np(&attr, 99, POSIX_SPAWN_PERSONA_FLAGS_OVERRIDE);
+    posix_spawnattr_set_persona_uid_np(&attr, 0);
+    posix_spawnattr_set_persona_gid_np(&attr, 0);
+
+    pid_t pid;
+    int status = posix_spawnp(&pid, argv[1], NULL, &attr, &argv[1], environ);
+
+    posix_spawnattr_destroy(&attr);
+
+    if (status != 0) {
+        fprintf(stderr, "failed to spawn %s: %s\n", argv[1], strerror(errno));
+        return 1;
+    }
+
+    waitpid(pid, &status, 0);
+    return WEXITSTATUS(status);
 }
